114bb273865a7b6741be57b0aab159d2979f85ba
gonzagaa <iigust4vo@gmail.com>
2024-09-17 15:31:02 -0300

    update

--- youtubedelay/delay.js
+++ youtubedelay/delay.js
@@ -1,5 +1,5 @@
  // Tempo manual do vídeo (em milissegundos). Por exemplo, 2 minutos = 120000 ms.
- const videoDuration = 900000; // Defina o tempo correto aqui.
+ const videoDuration = 840000; // Defina o tempo correto aqui.
 
  // Função para mostrar as seções após o vídeo
  function showSections() {
@@ -15,5 +15,23 @@
      document.getElementById("author").style.display = "block";
  }
 
+  // Função para incrementar o contador de pessoas
+  function incrementarContador() {
+    const spanContador = document.getElementById("contadorPessoas");
+    let contador = parseInt(spanContador.innerText);
+
+    // Incrementa o contador até atingir 1000
+    const intervalo = setInterval(() => {
+        if (contador < 1000) {
+            contador++;
+            spanContador.innerText = contador;
+        } else {
+            clearInterval(intervalo); // Para quando atingir 1000
+        }
+    }, 3879); // A cada 3.879 milissegundos
+}
+
  // Executar a função após o tempo determinado
  setTimeout(showSections, videoDuration);
+
+ incrementarContador();
